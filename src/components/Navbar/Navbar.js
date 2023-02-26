24087341fe0db1df8f6e822ed68fed8f5811fe84
walwizard <goneisgone@hotmail.com>
2023-02-26 11:18:52 -0300

    Bug Fixed :)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,17 +13,17 @@ const Navbar = () => {
 
     return (
         <div className="menu__nav">
-            <i class={isNavbar ?"bi bi-list lt" :"bi bi-x-lg lt" } onClick={() => setIsNavbar(!isNavbar)}></i>
-            
+            <i class={isNavbar ? "bi bi-list lt" : "bi bi-x-lg lt"} onClick={() => setIsNavbar(!isNavbar)}></i>
+
             <div className={isNavbar ? "navbar_invisible" : "navbar_visible"}>
-                
+
                 <div className="nav__itens">
                     <img src={require("../images/logo.png")} alt="logo" className="img__nav" />
 
-                    <li className="link__itens"><Link className="link__itens" to={"/"}>home</Link></li>
-                    <li className="link__itens"><Link className="link__itens" to={"/trailers"}>trailers</Link></li>
-                    <li className="link__itens"><Link className="link__itens" to={"/synopsis"}>synopsis</Link></li>
-                    <li className="link__itens"><Link className="link__itens" to={"/characters"}>characters</Link></li>
+                    <li ><Link className="link__itens" to={"/"}>home</Link></li>
+                    <li ><Link className="link__itens" to={"/trailers"}>trailers</Link></li>
+                    <li ><Link className="link__itens" to={"/synopsis"}>synopsis</Link></li>
+                    <li ><Link className="link__itens" to={"/characters"}>characters</Link></li>
                 </div>
             </div>
 
--- src/components/Trailer/Trailer1.js
+++ src/components/Trailer/Trailer1.js
@@ -3,7 +3,7 @@ import "./Trailer.css";
 
 const Trailer1 = () => {
     return (
-        <div className="right__side">
+        <div className="vide__section">
             <iframe key="KydqdKKyGEk" class="elementForYTiframe" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="The Super Mario Bros. Movie | Official Teaser Trailer" src="https://www.youtube-nocookie.com/embed/KydqdKKyGEk?rel=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.thesupermariobros.movie&amp;widgetid=15" id="widget16" width="100%" height="100%" frameborder="0"></iframe>
 
         </div>
